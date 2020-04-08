90bd1fe32ac2310c4deeced842695f792c273ebd
Leandro Cardozo <lcardozo@silentiumapps.com>
2020-04-08 11:01:23 -0300

    Refactorizacion

--- lib/main.dart
+++ lib/main.dart
@@ -46,115 +46,158 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Image.asset('assets/images/logo_guinart.png', height: 100, width: 200, alignment: Alignment.center,),
-
-            new Card(
-              margin: const EdgeInsets.only(left: 15.0, right: 15, top: 15),
-              shape: RoundedRectangleBorder(
-                side: BorderSide(color: Colors.white70, width: 1),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Container(
-                padding: const EdgeInsets.all(20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    Image.asset('assets/images/icon_user.png', height: 35, width: 35, alignment: Alignment.topLeft,),
-                    SizedBox(height: 5),
-                    Text('Username or e-mail',
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 14),
-                        textAlign: TextAlign.start),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                          hintText: 'Enter your email',
-                          hintStyle: TextStyle(fontSize: 12)),
-                      validator: (value) {
-                        if (value.isEmpty) {
-                          return 'Please enter some text';
-                        }
-                        return null;
-                      },
-                    ),
-                    SizedBox(height: 15),
-                    Image.asset('assets/images/icon_user.png', height: 35, width: 35, alignment: Alignment.topLeft,),
-                    SizedBox(height: 5),
-                    Text(
-                      'Password',
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 14),
-                      textAlign: TextAlign.start,
-                    ),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        hintText: 'Password',
-                        hintStyle: TextStyle(fontSize: 12),
-                      ),
-                      validator: (value) {
-                        if (value.isEmpty) {
-                          return 'Please enter some text';
-                        }
-                        return null;
-                      },
-                    ),
-                       SizedBox(height: 5),
-                     Text('Hide password',
-                        style: TextStyle(
-                            color: HexColor("#13356B"),
-                            fontWeight: FontWeight.w300,
-                            fontSize: 12),
-                        textAlign: TextAlign.start),
-                        SizedBox(height: 5),
-                     Text('I forgot my password',
-                        style: TextStyle(
-                            color: HexColor("#13356B"),
-                            fontWeight: FontWeight.normal,
-                            fontSize: 12),
-                        textAlign: TextAlign.end),
-                    SizedBox(height: 15),
-                    Container(
-                      height: 40.0,
-                      child: RaisedButton(
-                        onPressed: () {},
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10.0)),
-                        padding: EdgeInsets.all(0.0),
-                        child: Ink(
-                          decoration: BoxDecoration(
-                              gradient: LinearGradient(
-                                colors: [HexColor("#194B9C"), HexColor("#13356B")],
-                                begin: Alignment.topCenter,
-                                end: Alignment.bottomCenter,
-                              ),
-                              borderRadius: BorderRadius.circular(10.0)),
-                          child: Container(
-                            constraints: BoxConstraints(
-                                maxWidth: 300.0, minHeight: 50.0),
-                            alignment: Alignment.center,
-                            child: Text(
-                              "Login",
-                              textAlign: TextAlign.center,
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
+        child: Padding(
+          padding: EdgeInsets.all(25.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              _imageLogo(),
+              _card(),
+            ],
+          ),
         ),
       ),
     );
   }
+}
+
+_card() {
+  return Card(
+    shape: RoundedRectangleBorder(
+      side: BorderSide(color: Colors.white70, width: 1),
+      borderRadius: BorderRadius.circular(10),
+    ),
+    child: Padding(
+      padding: EdgeInsets.all(25.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          _imageUser(),
+          SizedBox(height: 5),
+          _usernameText(),
+          _usernameEntry(),
+          SizedBox(height: 15),
+          _imageUser(),
+          SizedBox(height: 5),
+          _passwordText(),
+          _passwordEntry(),
+          SizedBox(height: 5),
+          _hidePasswordText(),
+          _forgotPasswordText(),
+          SizedBox(height: 15),
+          _loginButton(),
+        ],
+      ),
+    ),
+  );
+}
+
+_usernameText() {
+  return Text('Username or e-mail',
+      style: TextStyle(
+          color: Colors.black, fontWeight: FontWeight.bold, fontSize: 14),
+      textAlign: TextAlign.start);
+}
+
+_usernameEntry() {
+  return TextFormField(
+    decoration: const InputDecoration(
+        hintText: 'Enter your email', hintStyle: TextStyle(fontSize: 12)),
+    validator: (value) {
+      if (value.isEmpty) {
+        return 'Please enter some text';
+      }
+      return null;
+    },
+  );
+}
+
+_passwordText() {
+  return Text(
+    'Password',
+    style: TextStyle(
+        color: Colors.black, fontWeight: FontWeight.bold, fontSize: 14),
+    textAlign: TextAlign.start,
+  );
+}
+
+_passwordEntry() {
+  return TextFormField(
+    decoration: const InputDecoration(
+      hintText: 'Password',
+      hintStyle: TextStyle(fontSize: 12),
+    ),
+    validator: (value) {
+      if (value.isEmpty) {
+        return 'Please enter some text';
+      }
+      return null;
+    },
+  );
+}
+
+_hidePasswordText() {
+  return Text('Hide password',
+      style: TextStyle(
+          color: HexColor("#13356B"),
+          fontWeight: FontWeight.w300,
+          fontSize: 12),
+      textAlign: TextAlign.start);
+}
+
+_forgotPasswordText() {
+  return Text('I forgot my password',
+      style: TextStyle(
+          color: HexColor("#13356B"),
+          fontWeight: FontWeight.normal,
+          fontSize: 12),
+      textAlign: TextAlign.end);
+}
+
+_loginButton() {
+  return Container(
+    height: 40.0,
+    child: RaisedButton(
+      onPressed: () {},
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+      padding: EdgeInsets.all(0.0),
+      child: Ink(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [HexColor("#194B9C"), HexColor("#13356B")],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+            borderRadius: BorderRadius.circular(10.0)),
+        child: Container(
+          constraints: BoxConstraints(minHeight: 50.0),
+          alignment: Alignment.center,
+          child: Text(
+            "Login",
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+_imageUser() {
+  return Image.asset(
+    'assets/images/icon_user.png',
+    height: 35,
+    width: 35,
+    alignment: Alignment.topLeft,
+  );
+}
+
+_imageLogo() {
+  return Image.asset(
+    'assets/images/logo_guinart.png',
+    height: 100,
+    width: 200,
+    alignment: Alignment.center,
+  );
 }
