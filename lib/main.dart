6a413b104528ffab339916accca7d5ad367512c8
Leandro Cardozo <lcardozo@silentiumapps.com>
2020-04-12 18:27:14 -0300

    Fix

--- lib/main.dart
+++ lib/main.dart
@@ -22,26 +22,99 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool _obscureText = true;
 
-  // bool _obscureText;
-
-  //  void stateSetter() {
-  //   setState(() {
-  //     _obscureText = !_obscureText;
-  //   });
-  // }
+  void stateSetter() {
+    setState(() {
+      _obscureText = !_obscureText;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-  
     return Scaffold(
-        drawer: NavDrawer(),
-        backgroundColor: Color(0xfffafafa),
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: _body(),
+      drawer: NavDrawer(),
+      backgroundColor: Color(0xfffafafa),
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints viewportConstraints) {
+        return SingleChildScrollView(
+          child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: viewportConstraints.maxHeight,
+              ),
+              child: Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    _imageLogo(),
+                    Card(
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(color: Colors.white70, width: 1),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.all(20.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: <Widget>[
+                            _imageUser(),
+                            SizedBox(height: 5),
+                            _usernameText(),
+                            _usernameEntry(),
+                            SizedBox(height: 10),
+                            _imageUser(),
+                            SizedBox(height: 5),
+                            _passwordText(),
+
+                            TextFormField(
+                              obscureText: _obscureText,
+                              decoration: const InputDecoration(
+                                hintMaxLines: 1,
+                                hintText: 'Password',
+                                hintStyle: TextStyle(fontSize: 12),
+                              ),
+                              validator: (value) {
+                                if (value.isEmpty) {
+                                  return 'Please enter some text';
+                                }
+                                return null;
+                              },
+                            ),
+
+                            SizedBox(height: 5),
+
+                            GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  _obscureText = !_obscureText;
+                                });
+                              },
+                              child: Text('Hide password',
+                                  style: TextStyle(
+                                      color: Color(0xff13356b),
+                                      fontWeight: FontWeight.w300,
+                                      fontSize: 12),
+                                  textAlign: TextAlign.start),
+                            ),
+
+                            _forgotPasswordText(),
+                            SizedBox(height: 10),
+                            _loginButton(),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )),
         );
+      }),
+    );
   }
 }
 
@@ -65,43 +138,15 @@ _scrollView(BoxConstraints viewportConstraints) {
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
               _imageLogo(),
-              _card(),
             ],
           ),
         )),
   );
 }
 
-_card() {
-  return Card(
-    shape: RoundedRectangleBorder(
-      side: BorderSide(color: Colors.white70, width: 1),
-      borderRadius: BorderRadius.circular(10),
-    ),
-    child: Padding(
-      padding: EdgeInsets.all(20.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          _imageUser(),
-          SizedBox(height: 5),
-          _usernameText(),
-          _usernameEntry(),
-          SizedBox(height: 10),
-          _imageUser(),
-          SizedBox(height: 5),
-          _passwordText(),
-          _passwordEntry(),
-          SizedBox(height: 5),
-          _hidePasswordText(),
-          _forgotPasswordText(),
-          SizedBox(height: 10),
-          _loginButton(),
-        ],
-      ),
-    ),
-  );
-}
+// _card() {
+//   return
+// }
 
 _usernameText() {
   return Text('Username or e-mail',
@@ -114,7 +159,7 @@ _usernameEntry() {
   return TextFormField(
     decoration: const InputDecoration(
         hintMaxLines: 1,
-        hintText: 'Enter your email', 
+        hintText: 'Enter your email',
         hintStyle: TextStyle(fontSize: 12)),
     validator: (value) {
       if (value.isEmpty) {
@@ -134,39 +179,35 @@ _passwordText() {
   );
 }
 
-_passwordEntry() {
-    return TextFormField(
-      obscureText: true,
-      decoration: const InputDecoration(
-        hintMaxLines: 1,
-        hintText: 'Password',
-        hintStyle: TextStyle(fontSize: 12),
-                             
+// _passwordEntry() {
+//     return TextFormField(
+//       obscureText: _obscureText,
+//       decoration: const InputDecoration(
+//         hintMaxLines: 1,
+//         hintText: 'Password',
+//         hintStyle: TextStyle(fontSize: 12),
 
-    ),
-    validator: (value) {
-      if (value.isEmpty) {
-        return 'Please enter some text';
-      }
-      return null;
-    },
-  );
-}
+//     ),
+//     validator: (value) {
+//       if (value.isEmpty) {
+//         return 'Please enter some text';
+//       }
+//       return null;
+//     },
+//   );
+// }
 
-_hidePasswordText()  {
-
-
-  return 
-  new GestureDetector(
-  onTap: (){},
-  child:   Text('Hide password',
-      style: TextStyle(
-          color: Color(0xff13356b),
-          fontWeight: FontWeight.w300,
-          fontSize: 12),
-      textAlign: TextAlign.start),
-);
-}
+// _hidePasswordText() {
+//   return new GestureDetector(
+//     onTap: () {},
+//     child: Text('Hide password',
+//         style: TextStyle(
+//             color: Color(0xff13356b),
+//             fontWeight: FontWeight.w300,
+//             fontSize: 12),
+//         textAlign: TextAlign.start),
+//   );
+// }
 
 _forgotPasswordText() {
   return Text('I forgot my password',
@@ -181,7 +222,7 @@ _loginButton() {
   return Container(
     height: 40.0,
     child: RaisedButton(
-      onPressed: (){},
+      onPressed: () {},
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
       padding: EdgeInsets.all(0.0),
       child: Ink(
